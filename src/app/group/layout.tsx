64fd464f5901fa10c83e885e32de9eecd85c1222
wintyo <wintyo1024@gmail.com>
2023-06-04 18:47:18 +0900

    タブページを1つ追加する

--- src/app/group/layout.tsx
+++ src/app/group/layout.tsx
@@ -13,7 +13,8 @@ const GroupLayout: FC<GroupLayoutProps> = ({ children }) => {
   return (
     <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
       <TabsLink>
-        <TabLink href="/group" label="タブ１" />
+        <TabLink href="/group" label="タブトップ" />
+        <TabLink href="/group/tab1" label="タブ１" />
         <TabLink href="/group/tab2" label="タブ２" />
         <TabLink href="/group/tab3" label="タブ３" />
       </TabsLink>
---
+++ src/app/group/tab1/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export type GroupTab1PageProps = {};
+
+const GroupTab1Page: FC<GroupTab1PageProps> = ({}) => {
+  return <>タブ１ページ</>;
+};
+
+export default GroupTab1Page;
